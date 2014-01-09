5b6d28f72a25b2fd0382e151fd351d9012c25f98
Keith Shaw <keith.w.shaw@gmail.com>
2014-01-08 21:31:14 -0800

    Updated wit from 2.13a to 2.24a.

--- Formula/wit.rb
+++ Formula/wit.rb
@@ -1,9 +1,9 @@
 require 'formula'
 
 class Wit < Formula
-  url 'http://wit.wiimm.de/download/wit-v2.13a-r4298-mac.tar.gz'
-  sha1 'deb2d7b4daaadab9e096799d428627bcb64b53f7'
-  version '2.13a'
+  url 'http://wit.wiimm.de/download/wit-v2.24a-r4723-mac.tar.gz'
+  sha1 '57f1eb2973aa0fdd27e67b11e8dd3a1c54a38e7a'
+  version '2.24a'
   homepage 'http://wit.wiimm.de/'
 
   def install
